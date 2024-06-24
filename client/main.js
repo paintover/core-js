236044ad3709d323f9dee5468423d6a6050d3681
paintover <tjdus1139@naver.com>
2024-06-24 17:38:41 +0900

    core js 커밋

--- client/main.js
+++ client/main.js
@@ -1,3 +1,35 @@
-const a = 'hello';
+import data from './data/data.js';
+import { getNode, getRandom, insertLast, clearContents } from './lib/index.js';
 
-console.log('안녕 웹브라우저!');
+// [phase-1]
+// 1. 주접 떨기 버튼을 클릭 하는 함수
+//    - 주접 떨기 버튼 가져오기
+//    - 이벤트 연결하기 addEventListener('click')
+
+// 2. input 값 가져오기
+//    - input.value
+
+// 3. data함수에서 주접 1개 꺼내기
+//    - data(name)
+//    - getRandom()
+
+// 4. pick 항목 랜더링하기
+
+const submit = getNode('#submit');
+const nameField = getNode('#nameField');
+const result = getNode('.result');
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const name = nameField.value;
+  const list = data(name);
+  const pick = list[getRandom(list.length)];
+
+  console.log(pick);
+
+  clearContents(result);
+  insertLast(result, pick);
+}
+
+submit.addEventListener('click', handleSubmit);
